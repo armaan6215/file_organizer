b2accb82c2ec1c28977a9c655cd3f5d8540ad96e
Manish Maharjan <manish@192-168-1-103.tpgi.com.au>
2021-12-04 19:11:39 +1100

    Implemented __name__==__main__

---
+++ file_organizer.py
@@ -0,0 +1,48 @@
+import os, shutil, mimetypes
+
+
+current_directory = os.getcwd()
+files = os.listdir(current_directory)
+file_types = ["audio", "image", "text", "video"]
+additional_extensions = {
+    "audio": [],
+    "image": [".webp"],
+    "text": [".docx", ".pdf"],
+    "video": [".mkv"],
+}
+
+
+def get_extensions_for_type(general_type):
+    for extension in mimetypes.types_map:
+        if mimetypes.types_map[extension].split("/")[0] == general_type:
+            yield extension
+
+
+def folder_handler(extensions_list, file_type):
+    for file in files:
+        name, ext = os.path.splitext(file)
+        if not os.path.exists(f"./{file_type}") and ext in extensions_list:
+            if ext != ".py":
+                os.makedirs(f"./{file_type}")
+        source = f"{current_directory}/{file}"
+        destination = f"{current_directory}/{file_type}/{file}"
+        if ext in extensions_list and ext != ".py":
+            try:
+                shutil.move(source, destination)
+            except Exception as e:
+                print(e)
+
+
+def main():
+    for file_type in file_types:
+        try:
+            extensions_list = (
+                list(get_extensions_for_type(file_type))
+                + additional_extensions[file_type]
+            )
+            folder_handler(extensions_list, file_type)
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    main()
